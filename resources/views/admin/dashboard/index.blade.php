e53c92a8be611cde2610fa5fbf01ebd09e0b7b6d
AcepHp <acephandika27@gmail.com>
2025-08-15 14:49:04 +0700

    feat: change frontView to model

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -132,7 +132,7 @@
                                             Line</th>
                                         <th
                                             class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                            Front View</th>
+                                            Model</th>
                                     </tr>
                                 </thead>
                                 <tbody class="bg-white divide-y divide-gray-200">
